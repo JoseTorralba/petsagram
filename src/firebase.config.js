551b77839d253dd38e8774dcc2977c88cbdce7b5
JoseTorralba <31675524+JoseTorralba@users.noreply.github.com>
2022-08-09 03:18:08 -0400

    started on project

---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFireStore } from 'firebase/firestore';
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyB_W7u68Gp2cyFFfOkg5ZTC3W3YMe9ahBA',
+  authDomain: 'petsagram-55fed.firebaseapp.com',
+  projectId: 'petsagram-55fed',
+  storageBucket: 'petsagram-55fed.appspot.com',
+  messagingSenderId: '714002607089',
+  appId: '1:714002607089:web:32471c762a44ccb8f33250',
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+export const db = getFireStore();
